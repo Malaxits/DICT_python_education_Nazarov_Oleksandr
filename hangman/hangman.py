fb9b5a25f317e42ac21f79d9313298831d0b80bf
Malaxits <o.i.nazarov@student.khai.edu>
2022-10-17 20:27:11 +0300

    hangman 2-st

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,13 @@
 import random
 
 
+
 print("HANGMAN")
 print("The game will be available soon")
+WORLDS = ["python", "java", "php", "javascript"]
+play = True
+while play:
+    menu = input('Type "play" to play the game, "exit" to quit:\n  ')
+    while menu not in ["play", "exit"]:
+        menu = input('Type "play" to play the game')
+
