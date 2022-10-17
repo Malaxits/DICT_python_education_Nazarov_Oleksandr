4ef1fb1d2fdbef4fa4f81e2b4723ebe2f9341999
Malaxits <o.i.nazarov@student.khai.edu>
2022-10-17 20:37:10 +0300

    hangman 3-st

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,5 +9,16 @@ play = True
 while play:
     menu = input('Type "play" to play the game, "exit" to quit:\n  ')
     while menu not in ["play", "exit"]:
-        menu = input('Type "play" to play the game')
+        menu = input('Type "play" to play the game, "exit" to quit:\n')
+    if menu == "play":
+        word = random.choice(WORLDS)
+        remember_letters = []
+        input_letters = []
+        all_letters = list(set(word))
+        words = "" .join([i if i in remember_letters else "" for i in word])
+        print(words)
+        live = 8
+        while live > 0:
+            letter = input("Input a letter:")
+
 
