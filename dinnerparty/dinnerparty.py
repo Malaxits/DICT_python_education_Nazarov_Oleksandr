38a7ac37b62895b9c6507378414779067d44136d
Malaxits <o.i.nazarov@student.khai.edu>
2022-10-23 16:32:46 +0300

    dinnerparty 2-st

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,5 +1,5 @@
 num = int(input("Enter the number of friends joining (including you):\n>"))
-if num  <1:
+if num<1:
     print('No one is joining for the party')
     exit()
 payments = {}
@@ -7,5 +7,11 @@ print('Enter the name of every friend (including you), each on a new line:')
 for _ in range(num):
     name = input(">")
     payments[name] = 0
+price = int(input("Enter the total amount:\n>"))
+a = round(price/num, 2)
+for name in payments:
+    payments[name] = a
 print(payments)
+
+
 
